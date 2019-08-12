b2b0950e0506dbb98fb7899364a4977280815387
Michael Castro <michael.castro@advantagesolutions.net>
2019-08-12 11:12:09 -0700

    Menu Item and Homepage Folder Items

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = () => (
+    
+    <div className='menu-item'>
+        <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>
+  
+);
+
+export default MenuItem;
