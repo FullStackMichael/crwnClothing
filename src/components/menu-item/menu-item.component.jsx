9333277645bbd9970f11bc12c69c045efe7c3df3
Michael Castro <michael.castro@advantagesolutions.net>
2019-08-15 08:52:11 -0700

    broken our homepade into smaller components, finished styling our menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = () => (
-    
-    <div className='menu-item'>
+const MenuItem = ({ title, imageUrl, size }) => (
+
+    <div className={`${size} menu-item`}>
+        <div className='background-image' style={{
+            backgroundImage: `url(${imageUrl})` //string interpolation of a parameter -- making the component dynamic
+        }} />
+
         <div className='content'>
-            <h1 className='title'>HATS</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
-  
+
 );
 
 export default MenuItem;
